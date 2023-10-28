47d2bfda86d3f30f86345220ffd729a895484970
Antonio6999 <antoine.chabeauti@gmail.com>
2023-10-28 12:15:17 +0200

    setup react + website base

---
+++ la-maison-jungle/src/components/App.js
@@ -0,0 +1,13 @@
+import Banner from "./Banner";
+import Cart from "./Cart";
+
+function App() {
+  return (
+    <div>
+      <Banner></Banner>
+      <Cart></Cart>
+    </div>
+  );
+}
+
+export default App;
